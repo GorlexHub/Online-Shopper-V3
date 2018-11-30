b850e8703b17970bc7b2a57737de9dbc1204c8a4
GorlexHub <alexandre.lovergne@gmail.com>
2018-11-30 14:27:17 +0100

    reprise V3

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+niceCat = Product.create(title: "Quality Kitty", 
+ subtitle: "Learn App Code", image_name: "kitty.JPG",
+ price: "4.99", sku: "kittyOne", download_url: "http://image.noelshack.com/fichiers/2018/48/3/1543397663-chat1.jpeg", 
+ details: "A really nice cat", description: %{<p>It's a cat</p>
+ })
